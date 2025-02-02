8b9972f0381c696864404d9d660169b4a2f826f0
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-02-02 13:23:16 +0000

    メソッド名を変更

--- ddd_common_lib/src/domain/model/DomainEventPublisher.php
+++ ddd_common_lib/src/domain/model/DomainEventPublisher.php
@@ -22,7 +22,7 @@ class DomainEventPublisher
 
     public function subscribe(DomainEventSubscriber $subscriber): void
     {
-        $this->addSubscrober($subscriber);
+        $this->addSubscriber($subscriber);
     }
 
     public function publish(DomainEvent $domainEvent): void
@@ -39,7 +39,7 @@ class DomainEventPublisher
         $this->subscriberList = [];
     }
 
-    private function addSubscrober(DomainEventSubscriber $subscriber): void
+    private function addSubscriber(DomainEventSubscriber $subscriber): void
     {
         $this->subscriberList[] = $subscriber;
     }
