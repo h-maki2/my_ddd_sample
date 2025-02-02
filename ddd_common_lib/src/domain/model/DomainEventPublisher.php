3785a4b731ae277e16f3da2513e484b1dcca593d
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-02-02 12:04:57 +0000

    publisherにメソッド追加

---
+++ ddd_common_lib/src/domain/model/DomainEventPublisher.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace dddCommonLib\domain\model;
+
+class DomainEventPublisher
+{
+    private static self $instance;
+
+    private array $subscriberList = []; // DomainEventSubscriber[]
+
+    private function __construct()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+    }
+
+    public static function instance(): self
+    {
+        return new self();
+    }
+
+    public function subscribe(DomainEventSubscriber $subscriber): void
+    {
+        $this->addSubscrober($subscriber);
+    }
+
+    public function publish(DomainEvent $domainEvent): void
+    {
+        foreach ($this->subscriberList as $subscriber) {
+            if ($subscriber->subscribedToEventType() === $domainEvent->eventType()) {
+                $subscriber->handleEvent($domainEvent);
+            }
+        }
+    }
+
+    public function reset(): void
+    {
+        $this->subscriberList = [];
+    }
+
+    private function addSubscrober(DomainEventSubscriber $subscriber): void
+    {
+        $this->subscriberList[] = $subscriber;
+    }
+}
