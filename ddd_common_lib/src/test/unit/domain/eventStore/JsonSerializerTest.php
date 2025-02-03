2133b8ccab090358f21d90c4288c4fa22244ec6a
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-02-03 12:11:49 +0000

    JsonSerializerの単体テストを追加

---
+++ ddd_common_lib/src/test/unit/domain/eventStore/JsonSerializerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use dddCommonLib\domain\eventStore\JsonSerializer;
+use dddCommonLib\test\helpers\event\TestEvent;
+use PHPUnit\Framework\TestCase;
+
+class JsonSerializerTest extends TestCase
+{
+    public function test_シリアライズしたオブジェクトをデシリアライズできる()
+    {
+        // given
+        $event = new TestEvent();
+
+        // when
+        // イベントをシリアライズする
+        $serializedEvent = JsonSerializer::serialize($event);
+
+        // シリアライズしたイベントをデシリアライズする
+        $deserializedEvent = JsonSerializer::deserialize($serializedEvent, TestEvent::class);
+
+        // then
+        $this->assertEquals($event->occurredOn()->format('Y-m-d'), $deserializedEvent->occurredOn()->format('Y-m-d'));
+        $this->assertEquals($event->eventVersion(), $deserializedEvent->eventVersion());
+        $this->assertEquals($event->eventType(), $deserializedEvent->eventType());
+    }
+}
