09fb78af21bab5848ffc2186bbbd7bb9fe280cae
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-02-04 16:08:30 +0000

    Exchangeクラスをリファクタリング

--- ddd_common_lib/src/adapter/messaging/rabbitmq/Exchange.php
+++ ddd_common_lib/src/adapter/messaging/rabbitmq/Exchange.php
@@ -2,31 +2,54 @@
 
 namespace dddCommonLib\adapter\messaging\rabbitmq;
 
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
 class Exchange
 {
-    readonly ConnectionSettings $connectionSettings;
     readonly string $exchangeName;
     readonly string $exchangeType;
-    readonly bool $durable;
+    readonly bool $isDurable;
+    readonly AMQPChannel $channel;
 
     private function __construct(
-        ConnectionSettings $connectionSettings,
         string $exchangeName,
         string $exchangeType,
-        bool $durable
+        bool $isDurable,
+        AMQPChannel $channel
     ) {
-        $this->connectionSettings = $connectionSettings;
         $this->exchangeName = $exchangeName;
         $this->exchangeType = $exchangeType;
-        $this->durable = $durable;
+        $this->isDurable = $isDurable;
+        $this->channel = $channel;
     }
 
     public static function fanOutInstance(
         ConnectionSettings $connectionSettings,
         string $exchangeName,
-        bool $durable
+        bool $isDurable
     ): self
     {
-        return new self($connectionSettings, $exchangeName, 'fanout', $durable);
+        $exchangeType = 'fanout';
+        $channel = self::exchangeDeclare($connectionSettings, $exchangeName, $isDurable, $exchangeType);
+        return new self($exchangeName, $exchangeType, $isDurable, $channel);
+    }
+
+    private static function exchangeDeclare(
+        ConnectionSettings $connectionSettings,
+        string $exchangeName,
+        bool $isDurable,
+        string $exchangeType
+    ): AMQPChannel
+    {
+        $connection = new AMQPStreamConnection(
+            $connectionSettings->hostName, 
+            $connectionSettings->port, 
+            $connectionSettings->userName, 
+            $connectionSettings->password
+        );
+        $channel = $connection->channel();
+        $channel->exchange_declare($exchangeName, $exchangeType, false, $isDurable, false);
+        return $channel;
     }
 }
