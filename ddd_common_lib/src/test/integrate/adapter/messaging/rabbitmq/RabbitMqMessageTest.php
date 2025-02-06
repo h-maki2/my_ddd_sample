332edba8446b743170b9179b13fff84b96211e40
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-02-06 16:13:54 +0000

    RabbitMqMessageTestにテストケースを追加

--- ddd_common_lib/src/test/integrate/adapter/messaging/rabbitmq/RabbitMqMessageTest.php
+++ ddd_common_lib/src/test/integrate/adapter/messaging/rabbitmq/RabbitMqMessageTest.php
@@ -2,6 +2,7 @@
 
 use dddCommonLib\adapter\messaging\rabbitmq\RabbitMqDeliveryMode;
 use dddCommonLib\adapter\messaging\rabbitmq\RabbitMqMessage;
+use dddCommonLib\adapter\messaging\rabbitmq\RabbitMqRetryCount;
 use PHPUnit\Framework\TestCase;
 
 class RabbitMqMessageTest extends TestCase
@@ -19,7 +20,74 @@ class RabbitMqMessageTest extends TestCase
         // then
         // 送信したメッセージが取得できる
         $this->assertEquals($sendingMessage, $rabbitMqMessage->value->body);
+
         // 送信したメッセージのデリバリーモードが取得できる
         $this->assertEquals($deliveryMode->value, $rabbitMqMessage->value->get('delivery_mode'));
+        
+        // 再送信の回数が0回であることを確認する
+        $headers = $rabbitMqMessage->value->get('application_headers');
+        $retryCount = $headers->getNativeData()[RabbitMqRetryCount::key()];
+        $this->assertEquals(0, $retryCount);
+
+        // 再送信の回数が最大回数に達していないことを確認する
+        $this->assertFalse($rabbitMqMessage->hasReachedMaxRetryCount());
+    }
+
+    public function test_エクスチェンジに再送信するメッセージを取得できる()
+    {
+        // given
+        // 送信するメッセージを取得する
+        $sendingMessage = json_encode(['test' => 'test message']);
+        $deliveryMode = RabbitMqDeliveryMode::PERSISTENT;
+        $rabbitMqMessage = RabbitMqMessage::get($sendingMessage, $deliveryMode);
+
+        // when
+        // 再送信するメッセージを取得する
+        $retrievedMessage = $rabbitMqMessage->retrieve();
+
+        // then
+        // 再送信するメッセージが取得できる
+        $this->assertEquals($sendingMessage, $retrievedMessage->value->body);
+
+        // 再送信するメッセージのデリバリーモードが取得できる
+        $this->assertEquals($deliveryMode->value, $retrievedMessage->value->get('delivery_mode'));
+
+        // 再送信の回数が1回であることを確認する
+        $headers = $retrievedMessage->value->get('application_headers');
+        $retryCount = $headers->getNativeData()[RabbitMqRetryCount::key()];
+        $this->assertEquals(1, $retryCount);
+
+        // 再送信の回数が最大回数に達していないことを確認する
+        $this->assertFalse($retrievedMessage->hasReachedMaxRetryCount());
+    }
+
+    public function test_エクスチェンジに再送信するメッセージが最大回数に達しているか確認できる()
+    {
+        // given
+        // 送信するメッセージを取得する
+        $sendingMessage = json_encode(['test' => 'test message']);
+        $deliveryMode = RabbitMqDeliveryMode::PERSISTENT;
+        $rabbitMqMessage = RabbitMqMessage::get($sendingMessage, $deliveryMode);
+
+        // when
+        // 3回再送信する
+        $retrievedMessage = $rabbitMqMessage->retrieve();
+        $retrievedMessage = $retrievedMessage->retrieve();
+        $retrievedMessage = $retrievedMessage->retrieve();
+
+        // then
+        // 再送信するメッセージが取得できる
+        $this->assertEquals($sendingMessage, $retrievedMessage->value->body);
+
+        // 再送信するメッセージのデリバリーモードが取得できる
+        $this->assertEquals($deliveryMode->value, $retrievedMessage->value->get('delivery_mode'));
+
+        // 再送信の回数が最大回数に達していることを確認する
+        $headers = $retrievedMessage->value->get('application_headers');
+        $retryCount = $headers->getNativeData()[RabbitMqRetryCount::key()];
+        $this->assertEquals(3, $retryCount);
+
+        // 再送信の回数が最大回数に達していることを確認する
+        $this->assertTrue($retrievedMessage->hasReachedMaxRetryCount());
     }
 }
