c6f7e396ef93301668845edd7cde47f087a61923
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-02-09 14:54:35 +0000

    ExchangeListenerを作成

--- ddd_common_lib/src/adapter/messaging/rabbitmq/ExchangeListener.php
+++ ddd_common_lib/src/adapter/messaging/rabbitmq/ExchangeListener.php
@@ -4,5 +4,51 @@ namespace dddCommonLib\adapter\messaging\rabbitmq;
 
 abstract class ExchangeListener
 {
-    
+    protected RabbitMqQueue $queue;
+    protected MessageConsumer $consumer;
+
+    public function __construct()
+    {
+        $this->attachToQueue();
+        $this->registerConsumer();
+    }
+
+    public function handle(): void
+    {
+        $this->consumer->listen();
+    }
+
+    abstract protected function exchangeName(): string;
+
+    abstract protected function queueName(): string;
+
+    abstract protected function connectionSettings(): ConnectionSettings;
+
+    abstract protected function  listensTo(): array;
+
+    abstract protected function filteredDispatch(): callable;
+
+    protected function queue(): RabbitMqQueue
+    {
+        return $this->queue;
+    }
+
+    protected function attachToQueue(): void
+    {
+        $this->queue = RabbitMqQueue::fromInstance(
+            $this->connectionSettings(),
+            $this->queueName(),
+            true
+        );
+    }
+
+    protected function registerConsumer(): void
+    {
+        $this->consumer = new MessageConsumer(
+            $this->queue,
+            $this->exchangeName(),
+            $this->listensTo(),
+            $this->filteredDispatch()
+        );
+    }
 }
