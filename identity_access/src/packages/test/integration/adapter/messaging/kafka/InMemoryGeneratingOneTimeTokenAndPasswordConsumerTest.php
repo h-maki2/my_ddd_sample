7c3012f7c10254cbf98824e76a04be91768e52f2
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-02-27 13:51:36 +0000

    修正

---
+++ identity_access/src/packages/test/integration/adapter/messaging/kafka/InMemoryGeneratingOneTimeTokenAndPasswordConsumerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use packages\adapter\persistence\inMemory\InMemoryDefinitiveRegistrationConfirmationRepository;
+use packages\application\registration\provisionalRegistration\GeneratingOneTimeTokenAndPasswordApplicationService;
+use packages\domain\model\email\IEmailSender;
+use packages\domain\model\email\SendEmailDto;
+use packages\test\helpers\transactionManage\TestTransactionManage;
+use Tests\TestCase;
+
+class InMemoryGeneratingOneTimeTokenAndPasswordConsumerTest extends TestCase
+{
+    private InMemoryDefinitiveRegistrationConfirmationRepository $definitiveRegistrationConfirmationRepository;
+    private TestTransactionManage $transactionManage;
+    private IEmailSender $emailSender;
+    private SendEmailDto $catchedSendEmailDto;
+    private GeneratingOneTimeTokenAndPasswordApplicationService $appService;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $emailSender = $this->createMock(IEmailSender::class);
+        $emailSender
+            ->method('send')
+            ->with($this->callback(function (SendEmailDto $sendEmailDto) {
+                $this->catchedSendEmailDto = $sendEmailDto;
+                return true;
+            }));
+        $this->emailSender = $emailSender;
+
+        $this->definitiveRegistrationConfirmationRepository = new InMemoryDefinitiveRegistrationConfirmationRepository();
+    }
+}
