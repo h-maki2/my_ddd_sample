07f6a8a1a5fbfe9c67b4d240330ed5651388690e
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-02-28 15:07:09 +0000

    修正

---
+++ identity_access/src/packages/test/helpers/domain/authenticationAccount/TestAuthenticationAccountCreatedFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace packages\test\helpers\domains\authenticationAccount;
+
+use packages\domain\model\authenticationAccount\AuthenticationAccountCreated;
+use packages\domain\model\authenticationAccount\IAuthenticationAccountRepository;
+use packages\domain\model\authenticationAccount\UserEmail;
+use packages\domain\model\authenticationAccount\UserId;
+use packages\test\helpers\domains\authenticationAccount\password\Md5PasswordManager;
+
+class TestAuthenticationAccountCreatedFactory
+{
+    private IAuthenticationAccountRepository $authenticationAccountRepository;
+    private TestAuthenticationAccountFactory $testAuthenticationAccountFactory;
+
+    public function __construct(
+        IAuthenticationAccountRepository $authenticationAccountRepository,
+        TestAuthenticationAccountFactory $testAuthenticationAccountFactory
+    )
+    {
+        $this->authenticationAccountRepository = $authenticationAccountRepository;
+        $this->testAuthenticationAccountFactory = $testAuthenticationAccountFactory;
+    }
+
+    public function create(
+        ?UserId $userId = null,
+        ?UserEmail $email = null,
+    ): AuthenticationAccountCreated
+    {
+
+        $authenticationAccountTestDataCreator = new AuthenticationAccountTestDataCreator(
+            $this->authenticationAccountRepository,
+            $this->testAuthenticationAccountFactory
+        );
+        $authenticationAccount = $authenticationAccountTestDataCreator->create(
+            email: $email ?? new UserEmail('test@example.com'),
+            id: $userId ?? $this->authenticationAccountRepository->nextUserId()
+        );
+
+        return new AuthenticationAccountCreated(
+            $authenticationAccount->id(),
+            $authenticationAccount->email()
+        );
+    }
+}
