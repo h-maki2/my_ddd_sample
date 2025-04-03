6016aa1fc7567c30419a32de167e4f51be958904
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-04-03 15:14:10 +0000

    削除してしまったコントローラを復元

---
+++ task_management/src/app/Http/Controllers/authentication/AuthenticationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\authentication;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use packages\application\login\LoginApplicationService;
+use packages\port\adapter\presenter\login\blade\BladeLoginPresenter;
+
+class AuthenticationController extends Controller
+{
+    private LoginApplicationService $loginApplicationService;
+
+    public function __construct(LoginApplicationService $loginApplicationService)
+    {
+        $this->loginApplicationService = $loginApplicationService;
+    }
+
+    public function displayLoginPage()
+    {
+        $loginUrl = $this->loginApplicationService->createLoginUrl();
+        return view('authentication.login', ['loginUrl' => $loginUrl]);
+    }
+
+    public function login(Request $request)
+    {
+        $this->loginApplicationService->login(
+            $request->query('code') ?? '',
+            $request->query('state') ?? ''
+        );
+
+        return redirect('/');
+    }
+}
--- task_management/src/routes/web.php
+++ task_management/src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\authentication\AuthenticationControllers;
+use App\Http\Controllers\authentication\AuthenticationController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\userProfile\CreateUserProfileController;
 use Illuminate\Support\Facades\Route;
@@ -9,8 +9,8 @@ Route::get('/', function () {
     print_r('Hello World');
 });
 
-Route::get('/login', [AuthenticationControllers::class, 'displayLoginPage']);
-Route::get('/auth/callback', [AuthenticationControllers::class, 'login']);
+Route::get('/login', [AuthenticationController::class, 'displayLoginPage']);
+Route::get('/auth/callback', [AuthenticationController::class, 'login']);
 
 Route::get('/profile/create', [CreateUserProfileController::class, 'displayForm']);
 Route::post('/profile/create', [CreateUserProfileController::class, 'create']);
