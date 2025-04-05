1255f4952aadce76e2bf0e9600d01ff6dbc9e422
former-pole-vaulter-hm <hyota.maki1998@gmail.com>
2025-04-05 15:31:01 +0000

    UserIdを修正

--- identity_access/src/packages/domain/model/authenticationAccount/UserId.php
+++ identity_access/src/packages/domain/model/authenticationAccount/UserId.php
@@ -11,12 +11,8 @@ class UserId
 
     public function __construct(string $value)
     {
-        if (!IdentifierFromUUIDver7::isValidLength($value)) {
-            throw new InvalidArgumentException('適切な文字列の長さではありません。');
-        }
-
-        if (!IdentifierFromUUIDver7::isValidFormat($value)) {
-            throw new InvalidArgumentException('適切な形式になっていません。');
+        if (empty($value)) {
+            throw new InvalidArgumentException('ユーザーIDが空です。');
         }
 
         $this->value = $value;
